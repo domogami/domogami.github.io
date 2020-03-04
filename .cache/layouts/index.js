9a1686b9112492285d322bd21b08b6356fb4915a
Dominick <d3lee@ucsd.edu>
2020-03-04 14:56:41 -0800

    Updates

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/domogami/Documents/GitHub/domogami.github.io/src/layouts/index.js"
-  import data from "/Users/domogami/Documents/GitHub/domogami.github.io/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
--- README.md
+++
@@ -1,3 +0,0 @@
-# Hey, I'm Dominick
-
-I am a second year currently in search of a software development internship. I have varied experience with Swift, Java, C, ARM, HTML, CSS, Javascript, React, and React Native. I hope to expand my skillsets and be an asset to an employer willing to provide me with an opportunity.
